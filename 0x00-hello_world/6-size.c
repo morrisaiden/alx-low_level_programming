5aec195a27faf379e9d47d8cdab55030ad111db9
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-08-17 12:26:11 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - A program that print a line using printf function
  *
- * Return: Always 0 (Success)
+ * Return:0 (Success)
  */
 int main(void)
 {
