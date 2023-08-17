ec1cabd1c45caf623af547864fe423006e5e3d36
morrisaiden <mwaimorris1997@gmail.com>
2023-08-17 01:38:57 -0700

    put program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * file: 4-puts.c
+ * Auth: morris <mwaimorris1997@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * prints  "programming is like building a multilingual puzzle"
+ * return: always  0.
+ */
+
+int main(void)
+{   
+	puts("\"Proputs("\"Programming is like building a multilingual puzzle");
+     
+	return (0);
+}
