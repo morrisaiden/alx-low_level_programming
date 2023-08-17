0664c98c8357607ab17637557ca9e59e436949bf
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-08-17 23:13:45 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,11 @@
  * Return: 0 (success)
  */
 int main(void)
-{
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
-	}
-
-	putchar('\n');
-
-	return (0);
+{char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
