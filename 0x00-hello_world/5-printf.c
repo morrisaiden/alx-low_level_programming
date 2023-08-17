7435841537048960ad96d7570b9f6cbefd4f6ef0
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-08-17 12:33:10 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
-/*
- * File: 5-printf.c
- * Auth: Morris G Mwai
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints "with proper grammar, but the outcome is
- *                a piece of art," followed by a new line.
+ * main - A program that prints a line using the printf function
  *
- * Return: Always 0.
+ * Return: 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+        return (0);
 }
