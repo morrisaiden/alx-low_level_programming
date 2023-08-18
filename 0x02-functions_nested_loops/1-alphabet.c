cd798e192d8e95d2614f5909146377c9c7875bba
morrisaiden <mwaimorris1997@gmail.com>
2023-08-18 15:32:29 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
+#include "main.h"
 
+/**
+ * 1-alphabet - prints alphatets i lower case followed by a new line
+ *
+ */
+
+void print_alphabets(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
