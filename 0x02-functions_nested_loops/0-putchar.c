910e756a7a9c9cad133426158a60b83d68037883
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-08-18 22:50:23 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
@@ -9,12 +8,14 @@
  */
 
 int main(void) {
-	char word[8] = "_putchar";
-	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	-putchar('\n');
 
-	for (i = 0; i < 7; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-	
 	return 0;
-}
