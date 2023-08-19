2af85ec38297b6321497a0ce8733f8cde43beb6a
morrisaiden <mwaimorris1997@gmail.com>
2023-08-19 00:14:28 -0700

    betty checked

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,8 @@
 /**
  * _isalpha - Checks if a character is alphabetic.
  *
+ * @c: The character to be checked
+ *
  * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
