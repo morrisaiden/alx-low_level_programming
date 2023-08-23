f0369801c0441e576318809a77a6c12e3d14a39a
morrisaiden <mwaimorris1997@gmail.com>
2023-08-23 12:35:01 -0700

    fix the error first then code

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -2,40 +2,40 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char s1[98];
-    char *ptr;
-    int i;
+  char s1[98];
+  char *p;
+  int i;
 
-    for (i = 0; i < 98 - 1; i++)
+  for (i = 0; i < 98 - 1; i++)
     {
-        s1[i] = '*';
+      s1[i] = '*';
     }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
+  s1[i] = '\0';
+  printf("%s\n", s1);
+  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+  printf("%s\n", s1);
+  printf("%s\n", p);
+  p = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+  printf("%s", s1);
+  printf("%s", p);
+  for (i = 0; i < 98; i++)
     {
-        if (i % 10)
+      if (i % 10)
         {
-            printf(" ");
+	  printf(" ");
         }
-        if (!(i % 10) && i)
+      if (!(i % 10) && i)
         {
-            printf("\n");
+	  printf("\n");
         }
-        printf("0x%02x", s1[i]);
+      printf("0x%02x", s1[i]);
     }
-    printf("\n");
-    return (0);
+  printf("\n");
+  return (0);
 }
