5c5ca1e11d82189d2c43049ee986d1068ccadfeb
morrisaiden <mwaimorris1997@gmail.com>
2023-08-23 09:16:14 -0700

    Noise is a buffer, more effective than a cubical or booth walls

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main -Entry point
  *
- * Return: Always 0.
+ * Return 0(success)
  */
+
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+  char s1[] = "Hello";
+  char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+  printf("%d\n", _strcmp(s1, s2));
+  printf("%d\n", _strcmp(s2, s1));
+  printf("%d\n", _strcmp(s1, s1));
+  return (0);
 }
