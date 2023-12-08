ca27382716c22a99a7f6a301e94e581b23aea7cb
morrisaiden <mwaimorris1997@gmail.com>
2023-12-08 10:53:16 -0800

    Add node

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * print_dlistint - a function that prints all the elements of a dlistint_t list
+ * print_dlistint - a function that prints all the
+   elements of a dlistint_t list
  * @h: head of doubly linked list
  * Return: number of nodes in list
  */
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdlib.h>
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL, *temp = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	new_node->n = n;
+
+	if (!head || !(*head))
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		temp = *head;
+		temp->prev = new_node;
+		*head = new_node;
+		new_node->next = temp;
+		return (new_node);
+	}
+	return (NULL);
+}
