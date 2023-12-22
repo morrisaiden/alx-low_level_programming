66c1cdc714aa08f215b35b6193b8dc87c734247b
morrisaiden <mwaimorris1997@gmail.com>
2023-12-22 10:53:13 -0800

    key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,7 +9,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index = 0;
-	
+
 	index = hash_djb2((unsigned char *)key);
-	return (index % size);	
+	return (index % size);
 }
