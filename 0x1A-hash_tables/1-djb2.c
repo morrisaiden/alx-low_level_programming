21f8dfaafa011869f258ee5dfd239c2096698d62
morrisaiden <139795199+morrisaiden@users.noreply.github.com>
2023-12-22 12:16:22 +0300

    1-djb2.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,9 @@
 #include "hash_tables.h"
 
-unsigned long int hash_djb2(const unsigned char *str)
-{
-    unsigned long int hash;
-    int c;
+/**
+  * hash_djb2 - A function that passed the key to the hash table.
+  * @str: The string to hash.
+  * Return: The calculated hash.
+  */
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
-}
+unsigned long int hash_djb2(const unsigned char *str)
