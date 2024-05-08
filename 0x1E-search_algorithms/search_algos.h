75ad24da59e37feed3f123ce1a4eaa9d88330bcd
morrisaiden <mwaimorris1997@gmail.com>
2024-05-08 09:19:16 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
 
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t i, size_t size);
+int jump_search(int *array, size_t size, int value);
 
 #endif
